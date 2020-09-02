a9b140508407084994dbcddef7d5ebdf9adff092
Lukey <ldipaola66@gmail.com>
2020-09-02 23:04:05 +1000

    minor changes and inc compression

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const compression = require('compression');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+app.use(compression());
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -13,10 +14,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true});
 
 
-app.use(require("./routes/api-routes.js"));
+require("./routes/api-routes.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
