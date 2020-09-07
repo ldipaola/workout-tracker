90f918f6fd35bde4ee1ed5f4b74f335e492aa97a
Lukey <ldipaola66@gmail.com>
2020-09-08 00:01:09 +1000

    connect heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require('compression');
+const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,9 +13,12 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true});
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(require("./routes/api-routes.js"));
 require("./routes/html-routes.js")(app);
